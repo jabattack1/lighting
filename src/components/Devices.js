7b4a61437d14647f8f45b53977241fe8e443ad64
Peter <peterhahn@Peters-MBP.fios-router.home>
2019-09-25 17:09:17 -0400

    notes

--- src/components/Devices.js
+++ src/components/Devices.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { ArcSlider, Box, Checkbox, Flex, Table, Txt } from 'rendition';
 import styled from 'styled-components';
 
+//pushing brightness levels from arc movements into this array
 var array = [];
-var brightnessArray = [];
 
 const ControlContainer = styled(Box)`
   border-top-left-radius: 10px;
@@ -19,12 +19,9 @@ const Room = styled.p`
   text-align: center;
 `;
 
-// TODO: Replace this with data loaded from the API
-
 class Devices extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = { data: props.data, id: '0' };
   }
 
@@ -87,9 +84,11 @@ class Devices extends React.Component {
   }
 
   doClick(row, e) {
-    console.log('row', this);
+    //toggle swtich ON/OFF when checkbox is clicked
     if (e.target.type == 'checkbox') {
       this.props.data[row.id - 1].active = !this.props.data[row.id - 1].active;
+
+      //retain brightness when switch is set to ON
       if (this.props.data[row.id - 1].active === true) {
         this.props.data[row.id - 1].brightness = this.props.data[
           row.id - 1
@@ -97,21 +96,25 @@ class Devices extends React.Component {
       }
     }
 
+    //deselect previously selected row when another row is selected
     for (var i = 0; i < this.props.data.length; i++) {
       if (this.props.data[i].selected === true && i !== row.id - 1) {
         this.props.data[i].selected = false;
       }
     }
 
+    //toggle selected true/false
     this.props.data[row.id - 1].selected = !this.props.data[row.id - 1]
       .selected;
 
+    //display selected row
     if (this.props.data[row.id - 1].selected === true) {
       document.getElementById('room').innerHTML = this.props.data[
         row.id - 1
       ].name;
     }
 
+    //when row is turned OFF deselect row, set brightness to 0, and save last brigthness setting
     if (this.props.data[row.id - 1].active === false) {
       this.props.data[row.id - 1].selected = false;
       this.props.data[row.id - 1].brightOn = this.props.data[
@@ -124,12 +127,15 @@ class Devices extends React.Component {
   }
 
   setPercentage(e) {
+    //push current brightness level to array
     var currentPercentage = this.props.data[this.state.id].brightness;
     array.push(currentPercentage);
   }
 
+  //arc movement
   doArc(e) {
     for (var k in this.props.data) {
+      //if row is active and selected set brightness
       if (
         this.props.data[k].active === true &&
         this.props.data[k].selected === true
@@ -137,12 +143,14 @@ class Devices extends React.Component {
         var activeLight = this.props.data[k];
         var percentage = Math.round(e / Math.pow(10, -2));
 
+        //no duplicate brigthness level pushed into array
         if (array[array.length - 1] !== percentage) {
           array.push(percentage);
         }
 
         activeLight.brightness = percentage;
 
+        //if brightness is at 0 and previous brightness level was at 1, turn row OFF
         if (
           percentage === 0 &&
           array[array.length - 2] === 1 &&
